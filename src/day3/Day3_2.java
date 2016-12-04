2930593c7edc77d3cca2c76879fe94b886fc8983
Doudouz <picheryc@gmail.com>
2016-12-04 01:31:12 +0100

    message

--- src/day3/Day3_2.java
+++ src/day3/Day3_2.java
@@ -5,25 +5,20 @@ import util.FileUtil;
 public class Day3_2 {
 
 	public static String[] input = FileUtil.readFileLines("res/day3/input.txt");
-	public static String[][] inputTable;
 
 	public static void main(String[] args) {
-		inputTable = new String[input.length][3];
-		for(int i = 0; i < input.length; i++){
-			inputTable[i] = input[i].trim().split("\\s+");
-		}
 		readcolumn(0,0,0);
 	}
 
 	private static void readcolumn(int i, int j, int possible) {
-		if(i > inputTable.length - 2){
+		if(i > input.length - 2){
 			readcolumn(0, j+1, possible);
 		}
-		else if(j == inputTable[0].length){
+		else if(j == 3){
 			System.out.println(possible);
 		}
 		else{
-			if(new Triangle(inputTable[i][j]+ " " + inputTable[i+1][j] + " " + inputTable[i+2][j]).isPossible()){
+			if(new Triangle(input[i].trim().split("\\s+")[j]+ " " + input[i+1].trim().split("\\s+")[j] + " " + input[i+2].trim().split("\\s+")[j]).isPossible()){
 				possible++;
 			}
 			readcolumn(i+3, j, possible);
